07e5c59726e9540bc081350dd4857f023afbb827
gfenderio <gfenderio@gmail.com>
2025-02-07 22:05:02 +0700

    First commit
    Co-authored-by: galangjrr <galangjrr@users.noreply.github.com>

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+date_default_timezone_set('Asia/Jakarta');
+
+$host  = 'localhost';
+
+git config --global user.name "gfenderio"
+git config --global user.email "gfenderio@gmail.com"
+
+
+
+
+
+
+?>
