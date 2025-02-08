c14d444dc6a6c6e9a6fe0207220dc02ffd031f32
galangjrr <galangnurfatwamaulana@gmail.com>
2025-02-08 17:11:13 +0700

    perubahan sidebar

--- template/sidebar.php
+++ template/sidebar.php
@@ -110,7 +110,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="<?= $main_url ?>user/add-user.php" class="nav-link">
+                    <a href="#" class="nav-link">
                       <i class="far fa-circle nav-icon"></i>
                       <p>Pengguna</p>
                     </a>
