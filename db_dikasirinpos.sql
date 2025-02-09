9b68ac03506f155c0c928b82416b676e8c9930c9
galangjrr <galangnurfatwamaulana@gmail.com>
2025-02-10 01:14:23 +0700

    db update db

--- db_dikasirinpos.sql
+++ db_dikasirinpos.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 07, 2025 at 05:59 PM
+-- Host: localhost
+-- Generation Time: Feb 09, 2025 at 07:13 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_supplier`
+--
+
+CREATE TABLE `tbl_supplier` (
+  `idsup` int(11) NOT NULL,
+  `nama` varchar(256) NOT NULL,
+  `telp` varchar(256) NOT NULL,
+  `deskripsi` varchar(256) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_user`
 --
 
@@ -36,6 +50,15 @@ CREATE TABLE `tbl_user` (
   `level` int(1) NOT NULL COMMENT '1 - administrator\r\n2 - supervisor\r\n3 - operator',
   `foto` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`userid`, `username`, `fullname`, `password`, `address`, `level`, `foto`) VALUES
+(2, 'tomoya', 'tomoya', '$2y$10$RTXclLzWOaH.Wx5unQlU.eGezi7wvUTrQjpQ7SJdlqc2G3pVRnRba', 'Jalan Kampung Cicadas No.114 Desa Cicadas, Kec. Gunung Putri', 1, '765-Tom.png'),
+(4, 'degea', 'David de Gea', '$2y$10$dgLChfo2TCSJMIHvgCZ/vu5GS0tfi7rpRFExmSYYKfeRiwYsxnlE2', 'Manchester Kabupaten', 2, '749-David_de_Gea_2017.jpg'),
+(5, 'arthur', 'Arthur Morgan', '$2y$10$kN/yn.aqqN2lk7hqAcQAWuQMZ6cNUwwLumhdUoE6EMZKSHQK.D2Ou', 'Blackwater', 3, '205-3061198-arthur portrait transparent.png');
 
 --
 -- Indexes for dumped tables
@@ -55,7 +78,7 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
